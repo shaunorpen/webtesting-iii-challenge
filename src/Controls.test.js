1a547b4eae49db0d7fc5459b1b94bc36b253038b
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2019-10-14 14:43:10 +0100

    Add Controls tests

--- src/Controls.test.js
+++ src/Controls.test.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import App from './Gate';
+import Controls from './Controls';
 
 afterEach(rtl.cleanup);
 
 let wrapper;
 
-beforeEach(() => {
-  wrapper = rtl.render(<App />);
-})
-
 describe('Controls component:', () => {
     it('renders correctly', () => {
-        expect(wrapper).toMatchSnapshot();
-      });
-// displays if gate is open/closed and if it is locked/unlocked
-// displays 'Closed' if the closed prop is true and 'Open' if otherwise
-// displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise
-// when locked or closed use the red-led class
-// when unlocked or open use the green-led class
+      wrapper = rtl.render(<Controls />);
+      expect(wrapper.container).toMatchSnapshot();
+    });
+
+    it('provides buttons to toggle the closed and locked states.', () => {
+      wrapper = rtl.render(<Controls />);
+      expect(wrapper.getByText(/Close Gate/)).toBeInTheDocument();
+      expect(wrapper.getByText(/Lock Gate/)).toBeInTheDocument();
+    });
+
+    it('button text changes to reflect the state the door will be in if clicked', () => {
+      wrapper = rtl.render(<Controls />);
+    });
+
+    it('the closed toggle button is disabled if the gate is locked', () => {
+      wrapper = rtl.render(<Controls locked={true}/>);
+      expect(wrapper.getByText(/Close Gate/)).toBeDisabled();
+    });
+
+    it('the locked toggle button is disabled if the gate is open', () => {
+      wrapper = rtl.render(<Controls open={true}/>);
+      expect(wrapper.getByText(/Lock Gate/)).toBeDisabled();
+    });
 });
